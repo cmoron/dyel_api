016039151ee8ae9847226686d291df8e577063df
Cyril Moron <cyril.moron@gmail.com>
2020-04-22 17:33:55 +0200

    Exercise model correction

--- src/models/exercise.ts
+++ src/models/exercise.ts
@@ -1,8 +1,9 @@
 import mongoose = require("mongoose");
 import ExerciseType from "./exerciseType";
+import ObjectId = mongoose.Schema.Types.ObjectId;
 
 export const ExerciseSchema =  new mongoose.Schema({
-    type: { type: ExerciseType, required: true },
+    type: { type: ObjectId, required: true },
     repeat: { type: String, required: true }
 });
 
