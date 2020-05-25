08abe0808f1cbd8cb738d4ec80f573491f8c2e5e
Cyril Moron <cyril.moron@gmail.com>
2020-05-25 21:49:43 +0200

    Gestion des erreurs de duplication sur save user.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,60 +2,64 @@ import { Request, Response } from "express";
 import User from '../models/user';
 import bcrypt from 'bcryptjs';
 
-export let addUser = (req: Request, res: Response) => {
+/*
+ * Function hashPassword.
+ * Returns hashed password with bcryptjs.
+ */
+async function hashPassword(password: string) {
+    const saltRounds = 10;
 
-    let {
-        name,
-        username,
-        email,
-        password,
-        confirm_password
-    } = req.body;
+    return await new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRounds, (err: Error, hash: string) => {
+            if (err) reject();
+            else resolve(hash);
+        });
+    });
+}
+
+/*
+ * Add new user in database.
+ * Handles errors for duplicate values.
+ */
+export let addUser = async (req: Request, res: Response) => {
+
+    /* Get request body. */
+    let { name, username, email, password, confirm_password } = req.body;
 
     /* Check password and confirmation password match. */
-    console.log("password: " + password + " // conf : " + confirm_password);
     if (password != confirm_password) {
         return res.status(400).json ({ message: "Passwords do not match." });
     }
 
-    /* Check username unicity. */
-    User.findOne({ username: username }).then(user => {
-        if (user) {
-            return res.status(400).json ({ message: "Username already exists." });
-        }
+    const hash = await hashPassword(password);
+
+    let user = new User({
+        name: name,
+        username: username,
+        email: email,
+        password: hash
     });
 
-    /* Check email unicity. */
-    User.findOne({ email: email }).then(user => {
-        if (user) {
-            return res.status(400).json ({ message: "Email already exists." });
-        }
-    });
+    user.save().then(user => {
+        console.log("user saved");
+        return res.status(201).json({
+            success: true,
+            message: "User is now registered."
+        });
+    }).catch(err => {
+        let errorMessage = "Error saving user."
 
-    /* Register user in database with hashed password. */
-    bcrypt.genSalt(10, (err: Error, salt: string) => {
-        bcrypt.hash(password, salt, (err: Error, hash: string) => {
-            if (err) throw err;
-            else {
-                let user = new User({
-                    name,
-                    username,
-                    email,
-                    hash
-                });
-
-                user.save().then(user => {
-                    return res.status(201).json({
-                        success: true,
-                        message: "User is now registered."
-                    });
-                }).catch(err => {
-                    return res.status(400).json({
-                        message: "Error saving user."
-                    });
-                });
+        /* Handle duplication errors. */
+        if (err.name === 'MongoError' && err.code == 11000) {
+            if (err.keyValue.username != null) {
+                errorMessage = 'username ' + username + ' already exists';
+            } else if (err.keyValue.email != null) {
+                errorMessage = 'email ' + email + ' already exists';
             }
+        }
+        return res.status(400).json({
+            success: false,
+            message: errorMessage
         });
     });
-
 };
--- src/models/user.ts
+++ src/models/user.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose"
 
 export const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    username: { type: String, required: true},
-    email: { type: String, required: true},
+    username: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
     date: { type: Date, default: Date.now }
 });
