e976f5d1819010ad2ce4b0f102f3e90b4bf913b5
Cyril Moron <cyril.moron@gmail.com>
2020-05-30 03:00:33 +0200

    Prise en compte app client lancée sur port 8080

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -25,6 +25,6 @@ export const corsOptions: cors.CorsOptions = {
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
   credentials: true,
   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: "http://localhost",
+  origin: "http://localhost:8080",
   preflightContinue: false
 };
